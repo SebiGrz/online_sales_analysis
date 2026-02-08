a6f12cca450e16f99523c57a81a6bea170f9c1ec
Sebi Groza <sebigroza31@outlook.com>
2026-02-08 22:49:24 +0200

    Update product q and name and deleted inv display

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ pm.add_product(Product("Mere", 1356, 5))
 pm.add_product(Product("Iepuri", 194, 10))
 pm.add_product(Product("Sarmale", 231, 7))
 
-pm.display_products()
-pm.total_inventory_value()
+pm.products[0].update_quantity(23)
+pm.products[1].name = "Castraveti"
 
 pm.remove_product("Iepuri")
 pm.display_products()
